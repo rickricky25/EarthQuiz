046478fe9ad871c2ff61c092f27d8d907944ba55
Ricky Effendi <rickricky.effendi@gmail.com>
2019-09-03 17:32:35 +0700

    add content

--- EarthQuiz/Quiz/QuizViewController.swift
+++ EarthQuiz/Quiz/QuizViewController.swift
@@ -10,13 +10,42 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    @IBOutlet weak var imgQuiz: UIImageView!
+    @IBOutlet weak var lblQuizQuestion: UILabel!
     @IBOutlet weak var lblQuizTitle: UILabel!
+    @IBOutlet weak var btnAnswerTop: UIButton!
+    @IBOutlet weak var btnAnswerBottom: UIButton!
+    
     var quizTitle = ""
     var level = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblQuizTitle.text = quizTitle
+        if level == 1 {
+            lblQuizTitle.text = "Level 1"
+            lblQuizQuestion.text = "Bonjour~\n\nI'm Paperino. I'm the famous eco-friendly product to replace plastic. Do you know where I come from?"
+            btnAnswerTop.setTitle("Forest", for: .normal)
+            btnAnswerBottom.setTitle("Farm", for: .normal)
+            imgQuiz.image = UIImage(named: "Paperbag1")
+        } else if level == 2 {
+            lblQuizTitle.text = "Level 2"
+            lblQuizQuestion.text = "Do you know how many trees to cut down to produce me?"
+            btnAnswerTop.setTitle("100.000 every day", for: .normal)
+            btnAnswerBottom.setTitle("100.000 every hour", for: .normal)
+            imgQuiz.image = UIImage(named: "Paperbag2")
+        } else if level == 3 {
+            
+        } else if level == 4 {
+            
+        } else if level == 5 {
+            
+        } else if level == 6 {
+            
+        } else if level == 7 {
+        
+        } else if level == 8 {
+            
+        }
     }
     
     @IBAction func btnAnswerPressed(_ sender: UIButton) {
